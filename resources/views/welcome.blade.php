0e3ff0d387ddc537c8b5a23622d7e4615da4c6ba
Gilberto Leandro <codexfast@gmail.com>
2020-02-17 13:43:24 -0300

    Add font and icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
 
         <title>Laravel</title>
 
