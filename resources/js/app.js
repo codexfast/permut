52b2b1827f75eb4d5515979ca0c848cc8484ac0c
Gilberto Leandro <codexfast@gmail.com>
2020-02-17 12:19:44 -0300

    Starter Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import vuetify from './vuetify'
+import App from './components/App.vue';
+import Layout from './components/Baseline.vue';
+
+const app = new Vue({
+    vuetify,
+    render: h => h(Layout),
+    el: '#app',
+});
